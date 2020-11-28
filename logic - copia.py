8d4c232c36ba9688b3ba7bf5e5bdaa67a17d36fa
marodriguezn <marodriguezn@uqvirtual.com>
2020-11-27 21:24:16 -0500

    Se modifico el archivo logic copia para probarlo en el script

--- logic - copia.py
+++ logic - copia.py
@@ -1,28 +1,30 @@
 print('AWS Hola mundo con Python')
 
 import requests
-import pandas as pd
 import boto3
+import json
 
-access_key  = 'AKIAQTCJUV7ZS32U2MP2'
-secret_access_key = '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'
+access_key  = 'AKIAQTCJUV7ZR6MRT5NN'
+secret_access_key = '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'
 
 # Consulta  a la base de datos
 url = 'https://pokeapi.co/api/v2/'
 response = requests.get(url+'pokemon?limit=1050&offset=1') #Trae todos los pokemon del 1 al 1050
 
-#Conversion a json, DataFrame y guardado en formato csv
+#Conversion a json
 r = response.json()
-df = pd.DataFrame(r)
-df.to_csv('/home/ubuntu/pokemon.csv')
+s = json.dumps(r, indent=4)
+f = open("/home/ubuntu/datos.json", "w")
+f.write(s)
+f.close()
 
 # Cliente con las credenciales
 client = boto3.client('s3', aws_access_key_id = access_key, aws_secret_access_key = secret_access_key)
 
 # Informacion necesaria para cargar al bucket
-ruta = '/home/ubuntu/pokemon.csv'
+ruta = '/home/ubuntu/datos.json'
 name_bucket = 'bucket1-desarrollo'
-save_route = 'Cargas/' + 'pokemon.csv'
+save_route = 'PrimerasCargas/' + 'datos.json'
 
 #Carga del archivo hacia el bucket
-client.upload_file('/home/ubuntu/pokemon.csv', name_bucket, save_route)
+client.upload_file(ruta, name_bucket, save_route)
